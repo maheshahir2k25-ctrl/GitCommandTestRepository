8b0f576945611faf36bc722447efff5f21199859
mahi7891122330 <m7891122330@gmail.com>
2025-12-22 16:42:10 +0530

    Revert "4 Files Commited"
    This reverts commit b2b6c8dbb3a1fdbb92009f4db72fec6e9613449b.

--- MVVM/MVVM/Model/User.cs
+++ MVVM/MVVM/Model/User.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MVVM.Model 
+namespace MVVM.Model
 {
     public class User
     {
--- MVVM/MVVM/ViewModel/UserViewModel.cs
+++ MVVM/MVVM/ViewModel/UserViewModel.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace MVVM.ViewModel 
+namespace MVVM.ViewModel
 {
 
     //public class UserViewModel : INotifyPropertyChanged
